34b91a45cdca8a09352f06dda0c047e794f02695
Dr. Martin Ahrnbom <m.ahrnbom@outlook.com>
2026-07-26 22:13:54 +0200

    a basic header, gotta start somewhere

--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
+from hyprland_schema import Schema
+from textual.app import App
+from textual.widgets import Header
+
 from schema import get_schema
+
+class UI(App):
+    def __init__(self, schema: Schema):
+        self.schema = schema 
+        super().__init__()
+
+        self.title = "Hyprland Settings TUI"
+        self.sub_title = "A TUI editor for hyprland.lua" 
+
+    def compose(self):
+        yield Header()
 
 def main():
     schema = get_schema()
-    print("Hello from hyprland-settings-tui!")
-    #print(schema)
+    ui = UI(schema=schema)
+    ui.run()
 
 if __name__ == "__main__":
     main()
