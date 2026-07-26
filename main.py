813ecae7f074e664a82c525e5a261dd03511f2bf
Dr. Martin Ahrnbom <m.ahrnbom@outlook.com>
2026-07-26 21:34:13 +0200

    started basic

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from hyprland-settings-tui!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/version.py
@@ -0,0 +1,34 @@
+# Determine the hyprland version
+
+import subprocess
+
+NUMBERS_AND_DOT = "0123456789."
+
+
+def determine_hyprland_version():
+    """
+    Returns None if the version cannot be determined
+    """
+
+    out = subprocess.run(["hyprland", "--version"], shell=False, stdout=subprocess.PIPE)
+    output = out.stdout.decode()
+    if not output.startswith("Hyprland "):
+        return None
+
+    words = output.split(" ")
+    if len(words) < 2:
+        return None
+
+    version = words[1]
+    if not version:
+        return None
+
+    for letter in version:
+        if letter not in NUMBERS_AND_DOT:
+            return None
+
+    return version
+
+
+if __name__ == "__main__":
+    print(determine_hyprland_version())
