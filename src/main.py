3f094b46bfcf44b2154c86649c35da6ef6405772
Dr. Martin Ahrnbom <m.ahrnbom@outlook.com>
2026-07-26 22:58:34 +0200

    basic tabbed interface for choosing section

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
+from typing import List
 from hyprland_schema import Schema
 from textual.app import App
-from textual.widgets import Header
+from textual.widgets import Header, TabbedContent, MarkdownViewer
 
 from schema import get_schema
+
+
+def get_main_sections(schema: Schema):
+    sections: List[str] = []
+    for opt in schema.options:
+        sec = opt.section[0]
+        if sec not in sections:
+            sections.append(sec)
+    return sections
 
 
 class UI(App):
@@ -13,10 +23,21 @@ class UI(App):
         self.title = "Hyprland Settings TUI"
         self.sub_title = "A TUI editor for hyprland.lua"
 
+        self.main_sections = get_main_sections(self.schema)
+        self.special_sections = ["monitors", "keybinds"]
+        self.all_sections = self.special_sections + self.main_sections
+
+    def build_pane(self, section: str):
+        return MarkdownViewer(section)
+
     def compose(self):
-        h = Header()
-        h.icon = "🔥"
-        yield h
+        header = Header()
+        header.icon = "🔥"
+        yield header
+
+        with TabbedContent(*self.all_sections):
+            for section in self.all_sections:
+                yield self.build_pane(section)
 
 
 def main():
