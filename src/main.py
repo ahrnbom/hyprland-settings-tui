5f939175046cfde7d4b8a3b7c4312594908c406a
Dr. Martin Ahrnbom <m.ahrnbom@outlook.com>
2026-07-27 07:43:20 +0200

    this is actually starting to look kind of nice

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 from typing import Dict, List
 from hyprland_schema import HyprOption, Schema
 from textual.app import App
+from textual.binding import Binding
 from textual.containers import HorizontalGroup, VerticalScroll
 from textual.widget import Widget
 from textual.widgets import (
     Checkbox,
     Collapsible,
+    DataTable,
     Header,
     Input,
     Label,
+    TabPane,
     TabbedContent,
 )
 
@@ -16,26 +19,13 @@ from schema import get_schema
 
 
 def get_sections(schema: Schema):
-    """
-    First output: A dictionary which maps all main sections to a list of its subsections (could be empty)
-    Second output: A dictionary mapping all main sections to a list of all options which don't have a subsection
-    """
-    secs: Dict[str, List[str]] = {}
-    nonsub: Dict[str, List[HyprOption]] = {}
+    secs: List[str] = []
     for opt in schema.options:
         sec = opt.section[0]
         if sec not in secs:
-            secs[sec] = []
-            nonsub[sec] = []
+            secs.append(sec)
 
-        if len(opt.section) == 2:
-            subsec = opt.section[1]
-            if subsec not in secs[sec]:
-                secs[sec].append(subsec)
-        elif len(opt.section) == 1:
-            nonsub[sec].append(opt)
-
-    return secs, nonsub
+    return secs
 
 
 def build_option_widget(opt: HyprOption):
@@ -69,7 +59,19 @@ def build_option_widget(opt: HyprOption):
     return Label(opt.name)
 
 
+def render_is_changed(is_changed: bool):
+    if is_changed:
+        return "O"
+    return "-"
+
+
 class UI(App):
+    BINDINGS = [
+        Binding("left", "prev_tab", "Previous Tab", priority=True),
+        Binding("right", "next_tab", "Next Tab", priority=True),
+    ]
+    CSS_PATH = "style.tcss"
+
     def __init__(self, schema: Schema):
         self.schema = schema
         super().__init__()
@@ -77,9 +79,9 @@ class UI(App):
         self.title = "Hyprland Settings TUI"
         self.sub_title = "A TUI editor for hyprland.lua"
 
-        self.schema_sections, self.nonsub = get_sections(self.schema)
+        self.schema_sections = get_sections(self.schema)
         self.special_sections = ["monitors", "keybinds"]
-        self.all_sections = self.special_sections + list(self.schema_sections.keys())
+        self.all_sections = self.special_sections + self.schema_sections
 
     def build_pane_list(self, section: str):
         out: List[Widget] = []
@@ -90,10 +92,6 @@ class UI(App):
                 for opt in self.schema.get_subsection(section, sub):
                     widgets.append(build_option_widget(opt))
                 out.append(Collapsible(*widgets, title=sub))
-
-            for opt in self.nonsub[section]:
-                out.append(build_option_widget(opt))
-
         else:
             out.append(Label(section))
 
@@ -106,9 +104,48 @@ class UI(App):
 
         with TabbedContent(*self.all_sections):
             for section in self.all_sections:
-                yield VerticalScroll(
-                    *self.build_pane_list(section), can_focus_children=True
-                )
+                yield self.make_table(section)
+
+    def make_table(self, section: str):
+        table = DataTable(name=section, zebra_stripes=True, cursor_type="row")
+        table.add_columns("Setting", "Changed", "Value", "Default", "Description")
+
+        for opt in self.schema.get_section(section):
+            name = opt.name
+            if len(opt.section) > 1:
+                parts = list(opt.section[1:])
+                parts.append(name)
+                name = ":".join(parts)
+
+            # TODO
+            is_changed = False
+            value = opt.default
+
+            table.add_row(
+                name, render_is_changed(is_changed), value, opt.default, opt.description
+            )
+
+        return table
+
+    def switch_tab(self, delta: int) -> None:
+        tabbed_content = self.query_one(TabbedContent)
+        panes = list(tabbed_content.query(TabPane))
+        if not panes:
+            return
+
+        current_id = tabbed_content.active
+        for i, pane in enumerate(panes):
+            if pane.id == current_id:
+                next_pane = panes[(i + delta) % len(panes)]
+                tabbed_content.active = next_pane.id
+                next_pane.query_one(DataTable).focus()
+                break
+
+    def action_next_tab(self) -> None:
+        self.switch_tab(1)
+
+    def action_prev_tab(self) -> None:
+        self.switch_tab(-1)
 
 
 def main():
